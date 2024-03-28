bf916a9b163cdadca86e5cda492b6a1a32aa2b60
AbdulWasaySultan <abdulwasay1408@gmail.com>
2024-03-29 03:04:28 +0500

    commmit

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-import inquirer from "inquirer";
-
-const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
-  { message: "Enter second number", type: "number", name: "secondnumber" },
-  {
-    message: "Select one of the operators to perform operation",
-    type: "list",
-    name: "operator",
-    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
-  },
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var inquirer_1 = require("inquirer");
+var answer = await inquirer_1.default.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondnumber" },
+    {
+        message: "Select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
 ]);
-
 if (answer.operator === "Addition") {
-  console.log(answer.firstNumber + answer.secondnumber);
-} else if (answer.operator === "Subtraction") {
-  console.log(answer.firstNumber - answer.secondnumber);
-} else if (answer.operator === "Multiplication") {
-  console.log(answer.firstNumber * answer.secondnumber);
-} else if (answer.operator === "Division") {
-  console.log(answer.firstNumber / answer.secondnumber);
-} else {
-  console.log("Enter a valid operator");
+    console.log(answer.firstNumber + answer.secondnumber);
 }
-
-console.log("Hi! faizan bhai");
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondnumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondnumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondnumber);
+}
+else {
+    console.log("Enter a valid operator");
+}
